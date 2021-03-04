0658d8bfd4b6c17fc9bb632773f61dc98036a3aa
JingNianNian <2020117103@stumail.nwu.edu.cn>
2021-03-04 22:44:46 +0800

    Update MainWindowOperate.cs

--- HaarAccelerator/MainWindowOperate.cs
+++ HaarAccelerator/MainWindowOperate.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Controls;
 namespace HaarAccelerator
 {
     class MainWindowOperate
     {
+        public static (int, int) ReplaceImage(Image image, int imageCount, int blocksCount, ClassifierChoose? classifierChecking)
+        {
+            //ToDo : 轮换图片并取消上一个图片的引用
+            return(1,1);
+        }
+        public static void GetClassifierModel()
+        {
+            //ToDo : 获取选定的分类器名
+        }
     }
 }
