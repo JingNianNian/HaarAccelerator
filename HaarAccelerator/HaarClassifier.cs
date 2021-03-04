ad5fb8cc53a8b5f14329ea4b632ad388b1e9a9eb
JingNianNian <2020117103@stumail.nwu.edu.cn>
2021-03-04 22:44:29 +0800

    Update HaarClassifier.cs

--- HaarAccelerator/HaarClassifier.cs
+++ HaarAccelerator/HaarClassifier.cs
@@ -8,5 +8,15 @@ namespace HaarAccelerator
 {
     class HaarClassifier
     {
+        //ToDo : 分类并存入tempblock
+        //ToDo : load图片完成后仅执行一次！
+
+    }
+    public enum ClassifierChoose
+    {
+        EyesClassifier,
+        EarsClassifier,
+        NoseClassifier,
+        MouthClassifier
     }
 }
