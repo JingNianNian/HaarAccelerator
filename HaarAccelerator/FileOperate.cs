593e128ca63011a15802a8a583bc3bd644eb132e
JingNianNian <2020117103@stumail.nwu.edu.cn>
2021-03-04 22:44:25 +0800

    Update FileOperate.cs

--- HaarAccelerator/FileOperate.cs
+++ HaarAccelerator/FileOperate.cs
@@ -8,5 +8,11 @@ namespace HaarAccelerator
 {
     class FileOperate
     {
+        //ToDo : 打开文件，存入imagepath
+        //ToDo : 复制文件并命名，存入temppath
+        //ToDo : 复制文件保存到程序目录下
+        //ToDo : 删除temp文件夹
+        //ToDo : 创建temp文件夹
+        //ToDo : 创建程序目录下文件夹
     }
 }
